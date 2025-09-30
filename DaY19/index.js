65199495d0544912858ad7d0fda4dcc122efdfa7
CHANDAN <kumardalaichandan803@gmail.com>
2025-09-30 19:47:52 +0530

    Add files via upload

---
+++ DaY19/index.js
@@ -0,0 +1,91 @@
+//NUmber]
+// let a = 10 ;
+// let b = 45.9876;
+// let c = b.toFixed(2);
+// console.log(c);
+// console.log(typeof(c));  // no string orginaal data is not changed 
+// console.log(a);
+// console.log(b.toFixed(2));
+
+
+// console.log(b.toPrecision(5));  //return string 
+// console.log(a.toString());
+
+
+
+// let a = new Number(20);
+// let b = new Number(20);
+// console.log(a)
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(a==b);
+
+// console.log(Boolean(new Number(10)));   //true bcz it is an object it is check only at refrence value exist 
+
+// let obj1 = {
+//     name: "Chandan",
+//     id : 53,
+// }
+
+// let obj2 = {
+//     name: "Chandan",
+//     id : 53,
+// }
+
+// console.log(obj1 == obj2);  //False bcz both are refering two different state if 
+// obj1 = obj2;
+// console.log(obj1 == obj2);  // return true 
+
+// console.log(Math.abs(-3));
+// console.log(Math.abs(86));
+// console.log(Math.max(4456,87));
+// let s = Number((Math.sin(45)));
+// console.log(Math.round(s));
+
+
+// console.log(Math.ceil(6.4));
+// console.log(Math.floor(6.4));
+
+
+// console.log(Math.random()); //Random value generated (0,1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Satte bazi game  Sponser by Chandan Maharana 
+// Choose your value from 0 to 9 
+// Choose your you want to add Money for that particular number
+// let c = (Math.random()*10+1);
+// console.log(Math.round(c));
+
+
+// console.log(Math.random()*6);
+
+
+// console.log(Math.floor(Math.random()*11)+15)
+
+// Math.floor(Math.random(max-min+1)+min)  // formula for random number in between two number
+// console.log(Math.floor(Math.random()*11)+20)  //20 to 30
+// console.log(Math.floor(Math.random()*11)+15)  //15 to 25 
+
+
+//OTP generator
+// console.log(Math.floor(Math.random()*(9999-1000+1))+1000);
+
+function random(){
+    
+}
